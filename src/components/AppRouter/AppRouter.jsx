8c3d2105aef3cb752327356cd936f7fae6e99710
LOiif <loiifmorner@gmail.com>
2022-05-31 00:16:59 +0300

    Поменял имя компонента страницы

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Converter from "../../pages/Converter/Converter";
+import ConverterPage from "../../pages/ConverterPage/ConverterPage";
 import Portfolio from "../../pages/Portfolio/Portfolio";
 import Page404 from "../../pages/404/Page404";
 
@@ -8,7 +8,7 @@ const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/converter" />} />
-      <Route path="/converter" element={<Converter />} />
+      <Route path="/converter" element={<ConverterPage />} />
       <Route path="/portfolio" element={<Portfolio />} />
       <Route path="/404" element={<Page404 />} />
       <Route path="*" element={<Navigate to="/404" />} />
