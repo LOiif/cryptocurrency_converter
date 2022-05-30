6d7b43fb9f3d420848b31a818af3d09938891742
LOiif <loiifmorner@gmail.com>
2022-05-31 00:24:17 +0300

    Добавлен объект с валютами и их id

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const currencies = {
+  bitcoin: {
+    name: 'Bitcoin',
+    id: 'bitcoin',
+    symbol: 'btc'
+  },
+  usd: {
+    name: 'US Dollar',
+    id: 'usd',
+    symbol: 'usd'
+  },
+  ether: {
+    name: 'Ether',
+    id: 'ethereum',
+    symbol: 'eth'
+  }
+}
