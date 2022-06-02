c6a94116b5fe46781fa7c9cc326d291bdb6a8e65
LOiif <loiifmorner@gmail.com>
2022-06-02 09:07:28 +0300

    Добавлены id выбранных валют и компонент графика

--- src/pages/ConverterPage/ConverterPage.jsx
+++ src/pages/ConverterPage/ConverterPage.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import cl from "./ConverterPage.module.scss";
 import Converter from "../../components/Converter/Converter";
+import Graph from "../../components/Graph/Graph";
 
 const ConverterPage = () => {
+  const [coinIdFrom, setCoinIdFrom] = useState("bitcoin");
+  const [coinIdTo, setCoinIdTo] = useState("bitcoin");
+
+  const changeCoins = (id, type) => {
+    if (type === "from") {
+      setCoinIdFrom(() => id);
+    } else if (type === "to") {
+      setCoinIdTo(() => id);
+    }
+  };
+
   return (
     <main className={cl.main}>
       <div className={cl.mainWrapper}>
-        <Converter/>
-        <div className={cl.plug}>
-        </div>
+        <Converter changeCoins={changeCoins} coinIdFrom={coinIdFrom} coinIdTo={coinIdTo} />
+        <Graph coinIdFrom={coinIdFrom} coinIdTo={coinIdTo} />
       </div>
     </main>
   );
