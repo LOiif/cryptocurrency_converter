2629232108aa1416359686c37a1927a3dd6222e5
LOiif <loiifmorner@gmail.com>
2022-06-04 19:49:32 +0300

    Сделана страница 404 при неверном пути

--- src/pages/404/Page404.jsx
+++ src/pages/404/Page404.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import cl from "./Page404.module.scss"
 
 const Page404 = () => {
-  return (
-    <>
-      <h1>Page is not found</h1>
-    </>
-  );
+    return (
+        <div className={cl.container}>
+            <h1 className={cl.title}>Страница не найдена...</h1>
+        </div>
+    );
 };
 
 export default Page404;
